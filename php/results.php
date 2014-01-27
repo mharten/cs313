f1d1da73e89b1e1a696a273177c1451cec4dadae
mharten <mike.harten@gmail.com>
2014-01-27 16:09:46 +0000

    adding php directory

---
+++ php/results.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+<title>Results</title>
+<link rel="stylesheet" type="text/css" href="/../style.css">
+</head>
+<html>
+<body>
+
+<?php
+echo "My first PHP script! Freak yeah!";
+?>
+
+</body>
+</html>
